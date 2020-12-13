d964bc6f6d11ab14ca7db071e6db14e2ef200343
Douglas Paz <oi@douglaspaz.com>
2020-12-12 23:08:38 -0300

    feat: add body parser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import expressWinston  from 'express-winston'
+import bodyParser from 'body-parser'
 import logger from './logger'
 import router from './router'
 
 const app = express()
 
 app.use(expressWinston.logger({ winstonInstance: logger }))
+app.use(bodyParser.json())
 app.use(router)
 
 export default app
