ebf7e883f3b218f386bf6f1a4680036b4b3a515c
Douglas Paz <oi@douglaspaz.com>
2020-12-15 20:54:43 -0300

    feat: add Channel constructor tests

--- src/channels/channel.js
+++ src/channels/channel.js
@@ -5,7 +5,10 @@ export default class Channel {
   static regex = /(?<key>[\w\W])+/
 
   constructor (uri) {
-    this.key = uri.match(this.constructor.regex).groups.key
+    const match = uri.match(this.constructor.regex)
+    if (!match) throw new Error(`"${uri}" not match with ${this.constructor.name} channel regex`)
+    if (!match.groups || !match.groups.key) throw new Error(`${this.constructor.name}.regex not has key in match group`)
+    this.key = match.groups.key
   }
 
   dispatchCode () {
--- test/channels/channel.test.js
+++ test/channels/channel.test.js
@@ -3,6 +3,34 @@ import Channel from '../../src/channels/channel'
 
 describe('channel', () => {
   describe('Channel', () => {
+    describe('constructor()', () => {
+      describe('not match URI', () => {
+        class NumberChannel extends Channel {
+          static _type = 'number-channel'
+          static regex = /(?<key>[\d]+)/
+        }
+
+        it('throw error', () => {
+          expect(() => {
+            new NumberChannel('abc')
+          }).to.throw('"abc" not match with NumberChannel channel regex')
+        })
+      })
+
+      describe('without key in channel regex', () => {
+        class InvalidChannel extends Channel {
+          static _type = 'invalid-channel'
+          static regex = /[\w\W]+/
+        }
+
+        it('throw error', () => {
+          expect(() => {
+            new InvalidChannel('uri')
+          }).to.throw('InvalidChannel.regex not has key in match group')
+        })
+      })
+    })
+
     describe('#dispatchCode', () => {
       it('throw NotImplemented', () => {
         const channel = new Channel('uri')
