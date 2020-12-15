256f254c7109714edae2e02dd76a3b0b2c66750d
Douglas Paz <oi@douglaspaz.com>
2020-12-14 22:27:11 -0300

    fix: test lint

---
+++ test/handlers/request.test.js
@@ -0,0 +1,49 @@
+import chai, { expect } from 'chai'
+import chaiHttp from 'chai-http'
+import mongoose from 'mongoose'
+import MongoMemoryServer from 'mongodb-memory-server'
+import app from '../../src/app'
+
+chai.use(chaiHttp)
+chai.should()
+
+describe('handlers/request', () => {
+  let mongod
+
+  beforeEach(async () => {
+    mongod = new MongoMemoryServer()
+    await mongoose.connect(
+      await mongod.getUri(),
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      }
+    )
+  })
+
+  afterEach(async () => {
+    await mongoose.disconnect()
+    await mongod.stop()
+  })
+
+  describe('POST /request', () => {
+    let response
+
+    beforeEach(async () => {
+      response = await chai.request(app)
+        .post('/request')
+        .type('json')
+        .send({
+          userURI: 'email:xuntos@dgls.me'
+        })
+    })
+
+    it('status code 201', () => {
+      response.should.have.status(201)
+    })
+
+    it('not render code', () => {
+      expect(response.body.code).to.be.undefined
+    })
+  })
+})
