a466ce1be080c3d13a0a71e325db8e9f226e687f
Douglas Paz <oi@douglaspaz.com>
2021-01-01 09:28:07 -0300

    improve: validateUserURI type

--- src/channels/utils.js
+++ src/channels/utils.js
@@ -1,6 +1,7 @@
 import { getChannel } from '.'
 
 export const validateUserURI = {
+  type: 'invalid_user_uri',
   validator: value => (!!getChannel(value)),
-  message: ({ value }) => (`${value} is not a valid URI.`)
+  message: ({ value }) => (`"${value}" is not a valid URI.`)
 }
