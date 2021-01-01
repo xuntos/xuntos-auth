a9d28cb28ef144d24df25e12c7f60e9329438735
Douglas Paz <oi@douglaspaz.com>
2021-01-01 09:28:26 -0300

    improve: add i18n default language

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,7 @@ import logger from './logger'
 import config from './config'
 
 export default new I18n({
+  defaultLocale: 'en',
   locales: ['en', 'pt-BR'],
   directory: config.i18n.localeDirectory,
   logDebugFn: logger.debug,
