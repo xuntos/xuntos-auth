799897617f203b42418d442b2b43f8f10be628fe
Douglas Paz <oi@douglaspaz.com>
2021-01-01 14:58:36 -0300

    feat: add dispatchCode to email channel

--- src/channels/email/index.js
+++ src/channels/email/index.js
@@ -1,5 +1,12 @@
+import path from 'path'
+import { renderTemplateFile } from 'template-file'
+import nodemailer from 'nodemailer'
 import Channel from '../channel'
 import config from '../../config'
+import i18n from '../../i18n'
+import logger from '../../../src/logger'
+
+const getTemplatePath = filename => (path.join(config.templatesDirectory, 'authentication-code/email', filename))
 
 export default class EmailChannel extends Channel {
   static _type = 'email'
@@ -7,5 +14,49 @@ export default class EmailChannel extends Channel {
 
   static activated () {
     return config.channels.email.enabled
+  }
+
+  static getTransporter () {
+    return nodemailer.createTransport(config.channels.email.smtp.uri)
+  }
+
+  async getSendMailOpts (authenticationRequest) {
+    const bodyTemplateData = {
+      yourAuthenticationCode: i18n.__({
+        phrase: 'Your authentication code is:',
+        locale: authenticationRequest.locale
+      }),
+      code: authenticationRequest.code,
+      footerMsg: i18n.__({
+        phrase: 'Use the authorization code to authenticate to the platform.',
+        locale: authenticationRequest.locale
+      })
+    }
+    return {
+      from: config.channels.email.from,
+      to: this.key,
+      subject: i18n.__({
+        phrase: 'Authentication Code',
+        locale: authenticationRequest.locale
+      }),
+      text: await renderTemplateFile(
+        getTemplatePath('body.txt'),
+        bodyTemplateData
+      ),
+      html: await renderTemplateFile(
+        getTemplatePath('body.html'),
+        bodyTemplateData
+      )
+    }
+  }
+
+  async dispatchCode(authenticationRequest) {
+    const sendMailOpts = await this.getSendMailOpts(authenticationRequest)
+    const mailInfo = await EmailChannel.getTransporter().sendMail(sendMailOpts)
+    logger.info(
+      `mail sent with authentication code to authentication request ${authenticationRequest.uuid}`,
+      { mailInfo }
+    )
+    return { success: true, mailInfo }
   }
 }
--- test/channels/email.test.js
+++ test/channels/email.test.js
@@ -1,8 +1,11 @@
 import '../support'
 
 import { expect } from 'chai'
+import sinon from 'sinon'
+import nodemailer from 'nodemailer'
 import config from '../../src/config'
 import EmailChannel from '../../src/channels/email'
+import AuthenticationRequest from '../../src/models/authentication-request'
 
 describe('EmailChannel', () => {
   describe('activated()', () => {
@@ -36,6 +39,46 @@ describe('EmailChannel', () => {
       it('returns false', () => {
         expect(EmailChannel.activated()).to.be.false
       })
+    })
+  })
+
+  describe('#getSendMailOpts()', () => {
+    let emailChannel
+    let authenticationRequest
+
+    beforeEach(() => {
+      emailChannel = new EmailChannel('email:xuntos@dgls.me')
+      authenticationRequest = new AuthenticationRequest()
+    })
+
+    it('expected structure', async () => {
+      const r = await emailChannel.getSendMailOpts(authenticationRequest)
+      expect(Object.keys(r)).deep.equal(['from', 'to', 'subject', 'text', 'html'])
+    })
+  })
+
+  describe('#dispatchCode()', () => {
+    let emailChannel
+    let authenticationRequest
+    let sandbox
+    let testAccount
+
+    beforeEach(async () => {
+      emailChannel = new EmailChannel('email:xuntos@dgls.me')
+      authenticationRequest = new AuthenticationRequest()
+      sandbox = sinon.createSandbox()
+      testAccount = await nodemailer.createTestAccount()
+      sandbox.stub(EmailChannel, 'getTransporter').returns(nodemailer.createTransport(`smtp://${testAccount.user}:${testAccount.pass}@smtp.ethereal.email:587`))
+    })
+
+    afterEach(() => {
+      sandbox.restore()
+    })
+
+    it('expected success true', async () => {
+      const r = await emailChannel.dispatchCode(authenticationRequest)
+      expect(r.success).to.be.true;
+      console.log(`see email: ${nodemailer.getTestMessageUrl(r.mailInfo)}`)
     })
   })
 })
