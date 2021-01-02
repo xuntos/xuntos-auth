26b429a330601518da3dd8e8961463cb1eda46fe
Douglas Paz <oi@douglaspaz.com>
2021-01-01 22:48:48 -0300

    improve: create authentication request logger

--- src/handlers/create-authentication-request.js
+++ src/handlers/create-authentication-request.js
@@ -2,13 +2,6 @@ import logger from '../logger'
 import AuthenticationRequest from '../models/authentication-request'
 
 export default async (req, res, next) => {
-  logger.info(
-    `authentication-request: ${JSON.stringify(req.body)}`,
-    {
-      type: 'authentication-request',
-      body: req.body
-    }
-  )
   const { userURI } = req.body || {}
   const authenticationRequest = new AuthenticationRequest({
     userURI,
@@ -16,6 +9,14 @@ export default async (req, res, next) => {
   })
   try {
     await authenticationRequest.save()
+    logger.info(
+      `authentication request created ${authenticationRequest.uuid} to ${authenticationRequest.userURI}`,
+      {
+        type: 'authentication-request',
+        uuid: authenticationRequest.uuid,
+        userURI: authenticationRequest.userURI
+      }
+    )
     res
       .status(201)
       .set('Content-Location', `/authentication-request/${authenticationRequest.uuid}`)
